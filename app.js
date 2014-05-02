79add41657fe4b0a1837d789ad52d4f7ad752b9e
Christian <christian.etpr10@gmail.com>
2014-05-02 10:26:05 -0400

    Extended demo to take data from one client and broadcast to others

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 var http = require('http')
+  , url = require('url')
   , fs = require('fs')
   , indexHtml = fs.readFileSync('./public/index.html')
+  , otroHtml = fs.readFileSync('./public/viewer.html')
   , BinaryServer = require('binaryjs').BinaryServer
   , binaryServer = BinaryServer({port: 9000});
 
 // Serv the static page..
 
 var server = http.createServer(function(request, response) {
+  var pathname = url.parse(request.url).pathname;
   response.writeHead(200);
-  response.end(indexHtml);
+  if (pathname == '/') {
+    response.end(indexHtml);  
+  } else if (pathname == '/otro') {
+    response.end(otroHtml);
+  };
+  
 });
 
 // WebServer listens on Port 4000
@@ -18,12 +26,16 @@ server.listen(4000);
 // Handle Binary WebSockets..
 
 binaryServer.on('connection', function(client) {
-
-  client.on('stream', function(stream) {
-
-      var responseStream = client.createStream('fromserver');
-      stream.pipe(responseStream);
-
+  client.on('stream', function(stream, meta) {
+    if (meta == 'toserver'){
+      for (var id in binaryServer.clients) {
+        var viewerClient = binaryServer.clients[id];
+        if (viewerClient != client) {
+          var send = viewerClient.createStream("fromserver");
+          stream.pipe(send);
+        };
+      };
+    };
   });
 
 });
