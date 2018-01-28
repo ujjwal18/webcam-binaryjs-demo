f8d56209a24695c77358241d68039659c6934247
Ujjwal-talentedge <ujjwal.kumar@talentedge.in>
2018-01-28 19:54:29 +0530

    working audio/video transmit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var http = require('http')
   , url = require('url')
   , fs = require('fs')
+  // ,ffmpeg = require('../index')
   // , indexHtml = fs.readFileSync('./public/index.html')
   // , otroHtml = fs.readFileSync('./public/viewer.html')
   , sendHtml = fs.readFileSync('./public/send.html')
@@ -12,6 +13,7 @@ var http = require('http')
 
 var server = http.createServer(function(request, response) {
   var pathname = url.parse(request.url).pathname;
+  // console.log(pathname);
   response.writeHead(200);
   // if (pathname == '/') {
   //   response.end(indexHtml);  
@@ -22,7 +24,15 @@ var server = http.createServer(function(request, response) {
     response.end(sendHtml);  
   } else if (pathname == '/recive') {
     response.end(reciveHtml);
-  };
+  }else {
+    try{
+      response.end(fs.readFileSync('./public/'+pathname));
+    }catch (error){
+      response.writeHead(404);
+      response.end();
+    }
+    
+  }
   
 });
 
@@ -31,12 +41,22 @@ var server = http.createServer(function(request, response) {
 server.listen(4000);
 
 // Handle Binary WebSockets..
+var broadcastStream = null ;
 
 binaryServer.on('connection', function(client) {
+  // client.on('close', function(code, message) {
+  //   console.log(code, message );
+  //   console.log(binaryServer.clients);
+  // });
+// console.log(client);
+  
+/*
   client.on('stream', function(stream, meta) {
     if (meta == 'toserver'){
+      console.log(binaryServer.clients);
       for (var id in binaryServer.clients) {
         var viewerClient = binaryServer.clients[id];
+        console.log(viewerClient);
         if (viewerClient != client) {
           var send = viewerClient.createStream("fromserver");
           stream.pipe(send);
@@ -44,5 +64,27 @@ binaryServer.on('connection', function(client) {
       };
     };
   });
+  */
+  client.on('stream', function(stream, meta) {
+    // console.log(meta);
+    if (meta == 'toserver'){
+      broadcastStream = stream ;
 
+      for (var id in binaryServer.clients) {
+        var viewerClient = binaryServer.clients[id];
+        // console.log(viewerClient);
+        if (viewerClient != client) {
+          var send = viewerClient.createStream("fromserver");
+          stream.pipe(send);
+        };
+      };
+    }else{
+      if(broadcastStream!= null){
+        var send = client.createStream("fromserver");
+        broadcastStream.pipe(send);
+      }
+      
+
+    }
+  });
 });
